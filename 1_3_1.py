4c026e3225b5ad79ca768a27cf4f558eca5f60ce
totaki <karagabul@gmail.com>
2015-12-22 21:48:42 +0300

    Success 1.3.1

--- 1_3_1.py
+++ 1_3_1.py
@@ -9,26 +9,37 @@ TEST_SETS = [
   ['4' , '-1 -2 -3 -4', '-5 -2 -1 -2', '0 2'],
   ['4' , '5 0 10 20', '0 40 0 30', '3 3'],
   ['6' , '1 1 1 7 1 10', '1 10 1 1 7 1', '3 4'],
+  ['4' , '2 1 2 3', '3 2 7 1', '0 2'],
+  ['10' , '4 -8 16 16 32 -6 0 56 -6 2', '-10 3 1 1 -5 4 -5 2 -1 -1', '7 7'],
+  ['4' , '7 6 5 4', '3 2 4 0', '0 2'],
 ] 
 
 
 def main(*args):
-    arr_i = arr_j = 0
+    arr_i = int(args[0]) - 1
     arr_a = args[1].split(' ')
     arr_b = args[2].split(' ')
-    maxs = []
-    max_i = [0, 0]
-    max_sum = sum([int(arr_a[max_i[0]]), int(arr_b[max_i[1]])])
+    maxs = [[int(arr_b[-1]), arr_i]]
+    arr_i -= 1
+    while arr_i >= 0:
+        num = int(arr_b[arr_i]) 
+        if num >= maxs[-1][0]:
+            maxs.append([num, arr_i])
+        else:
+            maxs.append(maxs[-1])
+        arr_i -= 1
+    arr_i = 1
+    temp_max = maxs.pop()
+    max_i = [0, temp_max[1]]
+    max_sum = sum([int(arr_a[max_i[0]]), temp_max[0]])
     while arr_i < int(args[0]):
-        arr_j = arr_i
-        while arr_j < int(args[0]):
-          num_a = int(arr_a[arr_i])
-          num_b = int(arr_b[arr_j])
-          cur_sum = sum([num_a, num_b])
-          if cur_sum > max_sum:
-              max_sum = cur_sum
-              max_i = [arr_i, arr_j]
-          arr_j += 1
+        temp_max = maxs.pop()
+        sum_temp = sum([int(arr_a[arr_i]), temp_max[0]])
+        if sum_temp > max_sum:
+            max_sum = sum_temp
+            max_i = [arr_i, temp_max[1]]
+        else:
+            pass
         arr_i += 1
     return ' '.join([str(i) for i in max_i])
 
