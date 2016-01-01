0456138b3bd961b953344377688948273d78c74e
totaki <karagabul@gmail.com>
2016-01-01 18:08:41 +0300

    Make temporary functions and tests this

--- 1_3_2.py
+++ 1_3_2.py
@@ -2,64 +2,99 @@ import math
 import sys
 
 
+"""
+Дан отсортированный массив различных целых чисел A[0..n-1] и массив целых чисел B[0..m-1].
+Для каждого элемента массива B[i] найдите минимальный индекс элемента массива A[k],
+ближайшего по значению к B[i].
+Время работы поиска для каждого элемента B[i]: O(log(k)).
+Подсказка. Обратите внимание, что время работы должно зависеть от индекса ответа - k. 
+Для достижения такой асимптотики предлагается для начала найти отрезок вида [2p,2p+1] , 
+содержащий искомую точку, а уже затем на нём выполнять традиционный бин. поиск.
+
+Sample Input:
+3
+10 20 30
+3
+9 15 35
+
+Sample Output:
+0 0 2
+"""
+
+
 TEST_SETS = [
-  ['4' , '4 -8 6 0', '-10 3 1 1', '0 1'],
-  ['3' , '1 6 5', '1 3 5', '1 2'],
-  ['4' , '4 4 5 5', '-10 4 3 5', '2 3'],
-  ['4' , '-1 -2 -3 -4', '-5 -2 -1 -2', '0 2'],
-  ['4' , '5 0 10 20', '0 40 0 30', '3 3'],
-  ['6' , '1 1 1 7 1 10', '1 10 1 1 7 1', '3 4'],
-  ['4' , '2 1 2 3', '3 2 7 1', '0 2'],
-  ['10' , '4 -8 16 16 32 -6 0 56 -6 2', '-10 3 1 1 -5 4 -5 2 -1 -1', '7 7'],
-  ['4' , '7 6 5 4', '3 2 4 0', '0 2'],
+  {'lena': '3' , 'arra': '10 15 35', 'lenb': '3','arrb': '5 7 30', 'result': '0 0 2'},
 ] 
 
 
-def main(*args, lena, arra, lenb, arrb):
-    arr_i = int(args[0]) - 1
-    arr_a = args[1].split(' ')
-    arr_b = args[2].split(' ')
-    maxs = [[int(arr_b[-1]), arr_i]]
-    arr_i -= 1
-    while arr_i >= 0:
-        num = int(arr_b[arr_i]) 
-        if num >= maxs[-1][0]:
-            maxs.append([num, arr_i])
-        else:
-            maxs.append(maxs[-1])
-        arr_i -= 1
-    arr_i = 1
-    temp_max = maxs.pop()
-    max_i = [0, temp_max[1]]
-    max_sum = sum([int(arr_a[max_i[0]]), temp_max[0]])
-    while arr_i < int(args[0]):
-        temp_max = maxs.pop()
-        sum_temp = sum([int(arr_a[arr_i]), temp_max[0]])
-        if sum_temp > max_sum:
-            max_sum = sum_temp
-            max_i = [arr_i, temp_max[1]]
-        else:
-            pass
-        arr_i += 1
-    return ' '.join([str(i) for i in max_i])
+TEST_SETS_GET_INDEXES = [
+    {'num': 2, 'result': [0, 1]},
+    {'num': 4, 'result': [1, 2]},
+]
 
 
-def test():
-    result = True
-    for i in TEST_SETS:
-        if main(*i[:3]) != i[3]:
-            result = False
-            print(main(*i[:3]), ' != ' , i[3])
+TEST_SETS_DIFF = [
+    {'index': 2, 'arr': [10,20,30], 'num': 25, 'result': 5},
+    {'index': 1, 'arr': [10,20,30], 'num': 25, 'result': 5},
+]
+
+
+def get_indexes(num, result=None):
+    num = int(num)
+    return [num // 2 - 1, num // 2]
+
+
+def diff(index, arr, num, result=None):
+    return abs(int(arr[index]) - int(num))
+
+
+def bin_search(len_, arr, num):
+    last_index = int(len_) - 1
+    cur_diff = lambda x: diff(x, arr, num)
+    indexes = get_indexes(last_index)
+    while indexes[0] >= 0 and indexes[1] <= last_index:
+        next_index = indexes[not cur_diff(indexes[0]) > cur_diff(indexes[1])]
+        # import pdb; pdb.set_trace()
+        result = next_index
+        indexes = get_indexes(next_index)
     return result
+
+
+def main(lena, arra, lenb, arrb, result=None):
+    arra = arra.split(' ')
+    arrb = arrb.split(' ')
+    result = ''
+    for i in arrb:
+        result += str(bin_search(lena, arra, i))
+    return ' '.join([str(i) for i in result])
+
+
+def test(test_sets, func):
+    success = True
+    for i in test_sets:
+        result = func(**i)
+        if result != i['result']:
+            success = False
+            print(result, ' != ' , i['result'])
+    return success
+
+
+def print_test(result):
+    print("Test - ", result and 'Ok')
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
-        print("Test - ", test() and 'Ok')
+        tests = [
+            test(TEST_SETS_GET_INDEXES, get_indexes),
+            test(TEST_SETS_DIFF, diff),
+            test(TEST_SETS, main)
+        ]
+        for i in tests:
+            print_test(i)
     else:
+        args = ['lena', 'arra', 'lenb', 'arrb']
         inputs = {}
-        inputs['lena'] = input('Input len a:')
-        inputs['arra'] = input('Input array a:')
-        inputs['lenb'] = input('Input len b:')
-        inputs['arrb'] = input('Input array b:')
+        for i in args:
+            inputs[i] = input('Input %s: ' % i)
         print(main(**inputs))
