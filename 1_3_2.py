d7448e254295da178a54dfd74f9d84127ab34b41
totaki <karagabul@gmail.com>
2016-01-12 07:47:11 +0300

    changes 1_3_2, broken on 10 test

--- 1_3_2.py
+++ 1_3_2.py
@@ -24,6 +24,13 @@ Sample Output:
 
 TEST_SETS = [
   {'lena': '3' , 'arra': '10 15 35', 'lenb': '3','arrb': '5 7 30', 'result': '0 0 2'},
+  {'lena': '3' , 'arra': '10 20 30', 'lenb': '3','arrb': '9 15 35', 'result': '0 0 2'},
+  {'lena': '3' , 'arra': '-10 5 20', 'lenb': '3','arrb': '-15 15 0', 'result': '0 2 1'},
+  {'lena': '3' , 'arra': '-10 -10 -1', 'lenb': '3','arrb': '-15 16 0', 'result': '0 2 2'},
+  {'lena': '3' , 'arra': '-10 0 5', 'lenb': '3','arrb': '-7 1 5', 'result': '0 1 2'},
+  {'lena': '3' , 'arra': '0 0 15', 'lenb': '3','arrb': '0 0 11', 'result': '0 0 2'},
+  {'lena': '4' , 'arra': '-15 -5 -1 2', 'lenb': '4','arrb': '5 -6 -11 0', 'result': '3 1 0 2'},
+  {'lena': '5' , 'arra': '10 15 35 35 50', 'lenb': '5','arrb': '10 7 40 20 30', 'result': '0 0 2 1 2'},
 ] 
 
 
@@ -33,30 +40,57 @@ TEST_SETS_GET_INDEXES = [
 ]
 
 
-TEST_SETS_DIFF = [
-    {'index': 2, 'arr': [10,20,30], 'num': 25, 'result': 5},
-    {'index': 1, 'arr': [10,20,30], 'num': 25, 'result': 5},
-]
+def noresult(func):
+    def wrapper(*args, **kwargs):
+        if 'result' in kwargs.keys():
+            del kwargs['result']
+        return func(*args, **kwargs)
+    return wrapper
 
 
-def get_indexes(num, result=None):
+@noresult
+def get_indexes(num):
     num = int(num)
-    return [num // 2 - 1, num // 2]
+    if num > 1:
+        return [num // 2 - 1, num // 2]
+    else:
+       return [0, 1]
 
 
-def diff(index, arr, num, result=None):
+@noresult
+def diff(index, arr, num):
     return abs(int(arr[index]) - int(num))
 
 
+@noresult
+def new_diff(arr, num, i, j):
+    i, j, num = (int(arr[i]), int(arr[j]), int(num))
+    result = False
+    diff_i, diff_j = (num - i, j - num)
+    if i == j and i > 0:
+        pass
+    elif diff_j < 0 or diff_i > diff_j:
+        result = True
+        # if num == 40:
+        #     import pdb; pdb.set_trace()
+    return result
+
+
 def bin_search(len_, arr, num):
-    last_index = int(len_) - 1
-    cur_diff = lambda x: diff(x, arr, num)
+    last_index = int(len(arr)) - 1
+    cur_diff = lambda x, y: diff(x, y, num)
     indexes = get_indexes(last_index)
-    while indexes[0] >= 0 and indexes[1] <= last_index:
-        next_index = indexes[not cur_diff(indexes[0]) > cur_diff(indexes[1])]
-        # import pdb; pdb.set_trace()
-        result = next_index
-        indexes = get_indexes(next_index)
+    offset = 0
+    while len(arr) >= 2:
+        index = new_diff(arr, num, *indexes)
+        next_index = indexes[index]
+        if index:
+            offset += (lambda: 0 if len(arr) <= 2 else indexes[1])()
+            arr = arr[indexes[1]:]
+        else:
+            arr = arr[:indexes[1]]
+        result = next_index + offset
+        indexes = get_indexes(len(arr) - 1)
     return result
 
 
@@ -87,7 +121,6 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
         tests = [
             test(TEST_SETS_GET_INDEXES, get_indexes),
-            test(TEST_SETS_DIFF, diff),
             test(TEST_SETS, main)
         ]
         for i in tests:
@@ -96,5 +129,5 @@ if __name__ == '__main__':
         args = ['lena', 'arra', 'lenb', 'arrb']
         inputs = {}
         for i in args:
-            inputs[i] = input('Input %s: ' % i)
+            inputs[i] = input()
         print(main(**inputs))
